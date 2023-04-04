c251dd7c40ddc8aff8d9b994fb782fbed5121cc4
dameldesign <98592632+dameldesign@users.noreply.github.com>
2023-04-04 10:54:49 +0100

    commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,50 @@
-import React from 'react';
+import React from "react";
+import CountUp from "react-countup"
+// intersection observer
+import { useInView } from "react-intersection-observer";
+// motion
+import { motion } from "framer-motion";
+// variant
+import { fadeIn } from "../variants";
 
 const About = () => {
+  const [ref, inView] = useInView({
+    threshold: 0.5,
+  });
   return (
-    <div className="section" id='about'>About</div>
+    <section className="section" id="about" ref={ref}>
+      <div className="container mx-auto">
+        <div>
+          {/* img */}
+          <div className="flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top">
+            img
+          </div>
+          {/* text */}
+          <div className="">
+            <h2>About me.</h2>
+            <h3>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Quisquam, velit?
+            </h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. A nemo in
+              vero dolorum exercitationem voluptaunde ea placeat corporis eos.
+              Aliquam, est!
+            </p>
+            {/* stats */}
+            <div className="flex">
+              <div>
+                <div className="text-[40px] font-tertiary text-gradient mb-2">
+                  {inView ? <CountUp start={0} end={13} duration={6}/>  :null}
+                </div>
+                <div className="font-primary text-sm tracking-[2px]">Years of <br/> Experience</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      About
+    </section>
   );
 };
 
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -12,54 +12,92 @@ import { fadeIn } from "../variants";
 
 const Banner = () => {
   return (
-    <section className="min-h-[85vh] lg:min-h-[78vh] flex items-center" id="home">
+    <section
+      className="min-h-[85vh] lg:min-h-[78vh] flex items-center justify-center"
+      id="home"
+    >
       <div className="container  max-auto">
-        <div className="flex flex-col gap-y-8 lg:items-center lg:gap-x-12">
+        <div className="flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12">
           {/* text */}
           <div className="flex-1 text-center font-secondary lg:text-left">
-            <h1 className="text-[55px] font-bold leading-[.8] lg:text-[110px]">
+            <motion.h1
+              variants={fadeIn("up", 0.3)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="text-[55px] font-bold leading-[.8] lg:text-[110px]"
+            >
               DAM<span>EL</span>
-            </h1>
-            <div className=" mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1] " >
+            </motion.h1>
+            <motion.div
+              variants={fadeIn("up", 0.4)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className=" mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1] "
+            >
               <span className=" text-white mr-4">I am a</span>
-              <TypeAnimation sequence={[
-                'Developer',
-                2000,
-                'Designer',
-                2000,
-              ]} 
-              speed={50}
-              className="text-accent"
-              wrapper="span"
-              repeat={Infinity}
+              <TypeAnimation
+                sequence={["Developer", 2000, "Designer", 2000]}
+                speed={50}
+                className="text-accent"
+                wrapper="span"
+                repeat={Infinity}
               />
-            </div>
-              <p className="mb-8 max-w-lg mx-auto lg:mx-0">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Quaerat, officia accusamus culpa nemo enim quam quas sunt nisi ipsam beatae.
-              </p>
-              <div className="flex max-w-max gap-x-6 items-center mb-12">
-                <button className="btn btn-lg">Contact Me</button>
-                <a href="#" className="text-gradient btn-link">My Portfolio</a>
-              </div>
-              {/* social */}
-              <div className="flex text-[20px] gap-x-6 max-w-max mx-auto">
-                <a href="#">
-                  <FaGithub/>
-                </a>
-                <a href="#">
-                  <FaYoutube/>
-                </a>
-                <a href="#">
-                  <FaDribbble/>
-                </a>
-              </div>
+            </motion.div>
+            <motion.p
+              variants={fadeIn("up", 0.7)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="mb-8 max-w-lg mx-auto lg:mx-0"
+            >
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat,
+              officia accusamus culpa nemo enim quam quas sunt nisi ipsam
+              beatae.
+            </motion.p>
+            <motion.div
+              variants={fadeIn("up", 0.6)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="flex max-w-max gap-x-6 items-center mb-12"
+            >
+              <button className="btn btn-lg">Contact Me</button>
+              <a href="#" className="text-gradient btn-link">
+                My Portfolio
+              </a>
+            </motion.div>
+            {/* social */}
+            <motion.div
+              variants={fadeIn("up", 0.7)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="flex items-start text-[20px] gap-x-6 max-w-max mx-auto"
+            >
+              <a href="#">
+                <FaGithub />
+              </a>
+              <a href="#">
+                <FaYoutube />
+              </a>
+              <a href="#">
+                <FaDribbble />
+              </a>
+            </motion.div>
           </div>
 
           {/* image */}
-          <div>
+          <motion.div
+            variants={fadeIn("down", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.7 }}
+            className="hidden lg:flex flex-1  max-w-[320px] lg:max-w-[482px]"
+          >
             <img src={Image} alt="" />
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
