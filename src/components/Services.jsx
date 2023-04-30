25cc1e4f31e3a0419d5b630a15641f1666fff154
dameldesign <dameldesign23@gmail.com>
2023-04-30 12:19:56 +0100

    initial commit

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,74 +1,98 @@
-import React from 'react';
+import React from "react";
 // icon
-import { BsArrowUpRight } from 'react-icons/bs'
+import { BsArrowUpRight } from "react-icons/bs";
 //motion
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 //variants
-import { fadeIn } from '../variants';
+import { fadeIn } from "../variants";
 
 //service data
 const services = [
   {
-    name:'UI/UX Design',
-    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam, velit?Quisquam, velit?',
-    link: 'Learn more',
+    name: "UI/UX Design",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam, velit?Quisquam, velit?",
+    link: "Learn more",
   },
   {
-    name:'Digital Marketing',
-    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam, velit?Quisquam, velit?',
-    link: 'Learn more',
+    name: "Digital Marketing",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam, velit?Quisquam, velit?",
+    link: "Learn more",
   },
   {
-    name:'Product Design',
-    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam, velit?Quisquam, velit?',
-    link: 'Learn more',
+    name: "Product Design",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam, velit?Quisquam, velit?",
+    link: "Learn more",
   },
-]
+];
 
 const Services = () => {
   return (
-    <section className="section" id='services'>
+    <section className="section" id="services">
       <div className="container mx-auto">
-        <div className='flex flex-col lg:flex-row'>
+        <div className="flex flex-col lg:flex-row">
           {/* text */}
-          <div className='flex-1 lg:bg-services lg:bg-bottom bg-no-repeat mix-blend-lighten mb-12 lg:mb-0'>
+          <motion.div
+            variants={fadeIn("right", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 lg:bg-services lg:bg-bottom bg-no-repeat mix-blend-lighten mb-12 lg:mb-0"
+          >
             <h2 className="h2 text-accent mb-6"> What I Do</h2>
             <h3 className="h3 max-w-[455px] mb-16">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
               Quisquam, velit?
             </h3>
-            <button className='btn btn-sm'>See my work</button>
-          </div>
+            <button className="btn btn-sm">See my work</button>
+          </motion.div>
           {/* services */}
-          <div className='flex-1'>
+          <motion.div
+            variants={fadeIn("left", 0.5)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1"
+          >
             {/* services list */}
             <div>
               {services.map((service, index) => {
                 //destructure service
-                const {name, description, link} = service;
+                const { name, description, link } = service;
                 return (
-                  <div className='border-b border-white/20 h-[145px] mb-[38px] flex' key={index}>
-                    <div className='max-w-[476px]'>
-                      <h4 className='text-[20px] tracking-wider font-primary font-semibold mb-6'>{name}</h4>
-                      <p className='font-secondary leading-tight'>{description}</p>
-                      </div>
-                    <div className='flex flex-1 flex-col items-end'>
-                      <a href="#" className='btn w-9 h-9 mb-[42px] flex justify-center items-center'>
-                        <BsArrowUpRight/>
+                  <div
+                    className="border-b border-white/20 h-[145px] mb-[38px] flex"
+                    key={index}
+                  >
+                    <div className="max-w-[476px]">
+                      <h4 className="text-[20px] tracking-wider font-primary font-semibold mb-6">
+                        {name}
+                      </h4>
+                      <p className="font-secondary leading-tight">
+                        {description}
+                      </p>
+                    </div>
+                    <div className="flex flex-1 flex-col items-end">
+                      <a
+                        href="#"
+                        className="btn w-9 h-9 mb-[42px] flex justify-center items-center"
+                      >
+                        <BsArrowUpRight />
                       </a>
-                      <a href="#" className='text-gradient text-sm'>{link}</a>
-                      </div>
+                      <a href="#" className="text-gradient text-sm">
+                        {link}
+                      </a>
+                    </div>
                   </div>
                 );
-
-
               })}
             </div>
-            </div>
+          </motion.div>
         </div>
-
       </div>
-      </section>
+    </section>
   );
 };
 
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,8 +1,51 @@
-import React from 'react';
+import React from "react";
+//motion
+import { motion } from "framer-motion";
+//variants
+import { fadeIn } from "../variants";
+
+//img
+import Img1 from "../assets/portfolio-img1.png";
+import Img2 from "../assets/portfolio-img2.png";
+import Img3 from "../assets/portfolio-img3.png";
 
 const Work = () => {
   return (
-    <div className="section" id='work'>Work</div>
+    <section className="section" id="work">
+      <div className="container mx-auto">
+        <div>
+          <div className="flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0 ">
+            {/* text */}
+            <div>
+              <h2 className="h2 leading-tight text-accent">
+                My Latest <br />
+                Work
+              </h2>
+              <p className="max-w-sn mb-16 ">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad,
+                eos quae amet repellat inventore obcaecati adipisci ratione sunt
+                fugit consequuntur.
+              </p>
+              <button className="btn btn-sm">View all Projects</button>
+            </div>
+            {/* image */}
+            <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
+              {/* overlay */}
+              <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all"></div>
+              {/* img */}
+              <img src={Img1} alt="" />
+              {/* pretitle */}
+              <div>UI/UX Design</div>
+              {/* title */}
+              <div>
+                <span>Project Title</span>
+              </div>
+            </div>
+          </div>
+          <div>2</div>
+        </div>
+      </div>
+    </section>
   );
 };
 
