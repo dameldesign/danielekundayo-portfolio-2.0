8a4e337c3b97a102d44fdbb5db6c2c15eda384d3
dameldesign <dameldesign23@gmail.com>
2023-05-09 08:20:06 +0100

    initial commit

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -13,7 +13,7 @@ const Work = () => {
   return (
     <section className="section" id="work">
       <div className="container mx-auto">
-        <div>
+        <div className="flex flex-col lg:flex-row gap-x-10">
           <div className="flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0 ">
             {/* text */}
             <div>
@@ -31,18 +31,20 @@ const Work = () => {
             {/* image */}
             <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
               {/* overlay */}
-              <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all"></div>
+              <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
               {/* img */}
-              <img src={Img1} alt="" />
+              <img className="group-hover:scale-125" src={Img1} alt="" />
               {/* pretitle */}
-              <div>UI/UX Design</div>
+              <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+                <span className="text-gradient"> UI/UX Design</span>
+              </div>
               {/* title */}
-              <div>
-                <span>Project Title</span>
+              <div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
+                <span className="text-3xl text-white">Project Title</span>
               </div>
             </div>
           </div>
-          <div>2</div>
+          <div className="flex-1">2</div>
         </div>
       </div>
     </section>
@@ -50,3 +52,4 @@ const Work = () => {
 };
 
 export default Work;
+ 
